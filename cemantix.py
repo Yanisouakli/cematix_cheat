86832b21a62621b5725ba9e567a0c617983c8a1e
Yanisouakli <ouakliyanis2000@outlook.fr>
2023-12-28 11:09:28 +0100

    added a function to handle the max issue

--- cemantix.py
+++ cemantix.py
@@ -25,7 +25,11 @@ import time, urllib.request
 import requests
 import pandas as pd 
 import numpy as np
+import locale
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.common.by import By
 
+locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
 def init(path):
     
@@ -40,10 +44,24 @@ def init(path):
 def get_similar_words(model, target_word):
     if target_word in model:
         # Get the top 10 most similar words
-        similar_words = model.most_similar(target_word, topn=15)
+        similar_words = model.most_similar(target_word, topn=8)
         return [word for word, _ in similar_words]
     else:
         return []
+
+
+def getMaxIndex(div):
+    great_number=[]
+    great=[]
+    for i in div:
+        time.sleep(2)
+        great_number.append(i.find_elements(By.XPATH,'//td[@class="number  "]'))
+    for i in range(len(div)*2 -1 ):
+        great.append(great_number[0][i].text)
+
+    great = [locale.atof(x) for x in great if x !='' ]
+    
+    return great.index(max(great))
 
 
 def submitWords(words,saved,count=0):
@@ -56,21 +74,28 @@ def submitWords(words,saved,count=0):
         wdw(driver,15).until(ec.element_to_be_clickable((By.CLASS_NAME,"guess-btn"))).click()
 
         
-    div = wdw(driver, 10).until(ec.presence_of_all_elements_located((By.XPATH, "//tbody[@class='guesses']/tr")))
-    great_word = div[0].find_elements(By.XPATH,'//td[@class="word close"]')
+    div = wdw(driver, 15).until(ec.presence_of_all_elements_located((By.XPATH, "//tbody[@class='guesses']/tr")))
+    
+    time.sleep(3)
+
+    index=getMaxIndex(div)
+    
+    great_word = div[index].find_elements(By.XPATH,'//td[@class="word "]')
     
     target_word=great_word[0].text
-
+    
+    print("great_word:",target_word)
     if saved==target_word:
         print("target revenue",target_word)
         count+=1
     if count > 4:
-        great_word = div[1].find_elements(By.XPATH,'//td[@class="word close"]')
+        great_word = div[1].find_elements(By.XPATH,'//td[@class="word "]')
     
         target_word=great_word[0].text
         print("target new",target_word)
 
     new_list=get_similar_words(model,target_word)
+    saved=target_word
     while True:
         submitWords(new_list,saved)
 
